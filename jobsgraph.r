a6c2f0bdd6e65fae3eae46f2b3bb02f9d71a6abd
Rui P. Chaves <33401801+RuiPChaves@users.noreply.github.com>
2021-10-31 16:55:29 -0400

    Add files via upload

--- jobsgraph.r
+++ jobsgraph.r
@@ -8,13 +8,16 @@ library(dplyr)
 j <- read.delim("jobs.txt", sep = ",")
 
 # Normalize area names
-levels(j$Area) <- c("CompLing","CompLing","Documentation","ForensicLing","HistoricalLing","Morphology","Neurolinguistics","Phonetics","Phonology","Pragmatics","Psycholinguistics","Semantics","Sociolinguistics","Syntax","Typology")
+levels(j$Area) <- c("CompLing","CompLing","Documentation","ForensicLing","HistoricalLing","Morphology","Neurolinguistics","Phonetics","Phonology","Pragmatics","Psycholinguistics","Semantics","Sociolinguistics","Syntax","Typology","Applied")
 
-# Forensic linguistics has less than 10 jobs (1997-2021) so, it is removed
+# Forensic linguistics has 153 jobs (1997-2021) so, it is removed
 j <- j[!(j$Area=="ForensicLing"),]
 
-# Typology has less than 30 jobs (1997-2021) so, it is removed
-j <- j[!(j$Area=="Typology"),]
+# Historical has 31 jobs (1997-2021) so, it is removed
+j <- j[!(j$Area=="HistoricalLing"),]
+
+# Applied linguistics has 164 jobs (1997-2021) so, it is removed
+j <- j[!(j$Area=="Applied"),]
 
 j$Area <- factor(j$Area) 
 
@@ -32,7 +35,7 @@ jto
 # Pie plot 
 ggplot(jto, aes(x="", y=Jobs, fill=Area)) +
   theme_bw(base_size=14) +
-  scale_fill_carto_d(name = "Area: ", palette = "Safe") +
+  #scale_fill_carto_d(name = "Area: ", palette = "Safe") +
   geom_bar(width = 1, stat = "identity") +
   coord_polar("y", start=0) +
   theme(axis.title.y=element_blank(), axis.text.y=element_blank(), axis.ticks.y=element_blank()) 
@@ -46,30 +49,61 @@ ggplot(jto, aes(x = Area,y = Jobs, group = Area, fill = factor(Area))) +
   #labs(title = "PostDoc job postings at https://linguistlist.org, per specialization area") +             
   theme_bw(base_size=14) +
   #scale_fill_colorblind(8) +
-  scale_fill_carto_d(name = "Area: ", palette = "Safe") +
-  theme(legend.title=element_blank(),legend.position = "bottom",
+#  scale_fill_carto_d(name = "Area: ", palette = "Safe") +
+  theme(legend.title=element_blank(),legend.position = "none",
         panel.background = element_rect(fill = "white")) 
 
 
 #########################################################################
 # Factor year
 
+
+# Nothing much happening in the 90's
+j <- j %>% filter(Year >= 2000)
+
+# So far...
+j <- j %>% filter(!(Month %in% c("November","December")))
+
 jt <- as.data.frame(aggregate(j$Jobs~j$Year + j$Area, FUN=sum))
 colnames(jt) <- c("Year","Area","Jobs") 
 jt$Year <- factor(jt$Year)
 
-    ggplot(jt, aes(x = Year, y = Jobs, group = Area, fill = factor(Area))) + 
+ggplot(jt, aes(x = Year, y = Jobs, group = Area, fill = factor(Area))) + 
      geom_histogram(stat="identity", color = "#000000",size=0.3) + 
       ylab("Job posts") +
+      xlab("") +
       #labs(title = "Overall job postings at https://linguistlist.org, per specialization area") +             
       labs(title = "Prof job postings at https://linguistlist.org, per specialization area") +             
       #labs(title = "PostDoc job postings at https://linguistlist.org, per specialization area") +             
       theme_bw(base_size=14) +
       #scale_fill_colorblind(8) +
       scale_fill_carto_d(name = "Area: ", palette = "Safe") +
-      theme(legend.title=element_blank(),legend.position = "bottom",
+      theme(legend.title=element_blank(),legend.position = "right",
               panel.background = element_rect(fill = "white")) 
   
+
+# Stacked, percentages 
+
+ggplot(j, aes(x = factor(Year), y=  Jobs, fill = factor(Area))) + 
+  geom_bar(position="fill", stat="identity",  size=0.3) + 
+  ylab("Job posts") +
+  xlab("") +
+  labs(title = "Prof job postings at https://linguistlist.org, per specialization area") +             
+  theme_bw(base_size=14) +
+  scale_fill_carto_d(name = "Area: ", palette = "Safe") +
+  theme(legend.title=element_blank(),legend.position = "right",
+        panel.background = element_rect(fill = "white")) 
+
+
+ggplot(jt, aes(x = Year,y = Jobs, group = Area, color=Area)) + 
+  geom_point(lwd = 2) + 
+  geom_line() +
+  labs(title = "Job postings at https://linguistlist.org") +             
+  theme_bw() +
+  scale_color_carto_d(name = "Area: ", palette = "Safe") +
+  ylab("Job posts") 
+
+
 
 ##############################################################################
 # Jobs across years, regardless of area
