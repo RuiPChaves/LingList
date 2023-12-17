796980488e4d67c9dcd40d48f2edc98f375a1b0a
Rui P. Chaves <33401801+RuiPChaves@users.noreply.github.com>
2023-12-17 10:18:58 -0500

    Add files via upload

--- raw_jobs.sh
+++ raw_jobs.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-egrep "[0-9]+, Jobs: " emails/*.txt | sed -E 's/emails\///' | sed -E 's/-.*//' | uniq -c | sed -E 's/ +//' > raw_jobs.csv
+egrep ", +Jobs: " emails/*.txt | sed -E 's/emails\///' | sed -E 's/-.*//' | uniq -c | sed -E 's/ +//' > raw_jobs.csv
+
 
